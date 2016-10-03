4f4f965cc74945939d563e18ed685ec4f3df657f
Madusudanan.B.N <b.n.madusudanan@gmail.com>
2016-10-03 12:09:53 +0530

    Update FindElementInArray.java

--- FindElementInArray.java
+++ FindElementInArray.java
@@ -15,5 +15,33 @@ public class FindElementInArray {
 
         return -1;
     }
+    
+     /**
+     * Uses the binary search mechanism to find an element in O(logn) time
+     * The algorithm assumes that the passed in data array is already sorted
+     * @param data_array
+     * @param numberToBeFound
+     * @return
+     */
+    public static int findElementInSortedArray(int data_array[],int numberToBeFound){
+
+        int lower_bound = 0;
+        int upper_bound = data_array.length - 1;
+        int middle = 0;
+        while (lower_bound <= upper_bound) {
+            middle = (lower_bound + upper_bound) / 2;
+            if (numberToBeFound < data_array[middle]) {
+                upper_bound = middle - 1;
+            } else if (numberToBeFound > data_array[middle]) {
+                lower_bound = middle + 1;
+            } else {
+                //Element found
+                return middle;
+            }
+        }
+        // Return -1 if not found
+        return -1;
+
+    }
 
 }
