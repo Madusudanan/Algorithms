f24653d176a243fc3008a77edcd19b2c5b0ea549
Madusudanan.B.N <b.n.madusudanan@gmail.com>
2016-10-03 12:03:59 +0530

    Update FindElementInArray.java

--- FindElementInArray.java
+++ FindElementInArray.java
@@ -1,11 +1,12 @@
-/**
- * Brute force way to find an element in an array
- * Keep in mind that this method is the best way to find an element if the given array is not sorted
- * O(n) complexity
- */
+
 public class FindElementInArray {
 
-    public static int findElement(int data_array[],int numberToBeFound){
+    /**
+     * Brute force way to find an element in an array
+     * Keep in mind that this method is the best way to find an element if the given array is not sorted
+     * O(n) complexity
+    */
+    public static int findElementBruteForce(int data_array[],int numberToBeFound){
         for(int i=0;i<data_array.length;i++){
             if(data_array[i] == numberToBeFound){
                 return i;
